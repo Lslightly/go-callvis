3fa9685406ff2a3f234edb5efd98fc0976786397
Lqw <lqw332664203@mail.ustc.edu.cn>
2022-03-06 17:29:19 +0800

    interface在参数，返回值，goroutine中调用

--- examples/src/interface_any_position/main.go
+++ examples/src/interface_any_position/main.go
@@ -5,9 +5,14 @@ type Inf interface {
 }
 
 type S struct{}
+type T struct{}
 
 func (s S) f() {
 	println("S's f()")
+}
+
+func (t T) f() {
+	println("T's f()")
 }
 
 func passInf(inf *Inf) *Inf {
@@ -18,10 +23,18 @@ func passInf(inf *Inf) *Inf {
 func main() {
 	var inf Inf = &S{}
 	(*passInf(&inf)).f()
-	ch := make(chan bool)
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
 	go func() {
 		inf.f()
-		ch <- true
+		ch1 <- true
 	}()
-	<-ch
+	go func() {
+		(*passInf(&inf)).f()
+		ch2 <- true
+	}()
+	<-ch1
+	<-ch2
+	inf = &T{}
+	inf.f()
 }
